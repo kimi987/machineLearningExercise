4696b624ceb060e1f8697f442d078a87f01587a5
cckwjj <cckwjj@hotmail.com>
2019-03-07 13:54:40 +0800

    'modify'

--- week1/gradientDescentMulti.m
+++ week1/gradientDescentMulti.m
@@ -1,16 +1,10 @@
-function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
   m = length(y);
   J_history = zeros(num_iters, 1);
   n = length(theta);
   theta_t = theta;
   for iter = 1:num_iters
-    for j=1:n
-      theta_t(j,1) = theta(j,1) - alpha * sum((X *theta - y).*X(:,j))/m;
-      
-    end
-   
-  theta = theta_t;
- 
-  J_history(iter) = computerCostMulti(X, y, theta); 
+    theta= theta - alpha * ((X * theta - y)' *X / m)';
+    J_history(iter) = computerCostMulti(X, y, theta); 
   endfor
 endfunction
